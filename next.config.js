14239a69a62afb19f2141567b2e644cbec80dce5
Rustam650 <spaces553@mail.ru>
2025-04-08 22:42:12 +0300

    Улучшено обнаружение и создание index.html для деплоя

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     domains: ['example.com', 'images.unsplash.com', 'source.unsplash.com', 's3.twcstorage.ru', 'storage.timeweb.com'],
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60,
+    unoptimized: true,
   },
   output: 'export',
   distDir: '.next',
